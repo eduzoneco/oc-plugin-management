d52df290cddde3198e49b66c1ad046ac847e7ed5
Sebastian Alzate <sebastianab100@gmail.com>
2023-04-12 20:14:59 -0500

    traduction coursecategory

--- lang/en/lang.php
+++ lang/en/lang.php
@@ -8,11 +8,21 @@
         'coursecategory' => 'Course Category',
         'course' => 'Course'
     ],
+    'general' =>[
+        'created_at' => 'Created at',
+        'updated_at' => 'Updated at',
+        'deleted_at' => 'Deleted at'
+    ],
     'coursecategory' => [
         'form' => [
+            'name'=> 'Name',
+            'description'=> 'Description',
 
         ],
         'list' =>[
+            'id' => 'Id',
+            'name' => 'Name',
+            'description' => 'Description'
 
         ]
     ]
